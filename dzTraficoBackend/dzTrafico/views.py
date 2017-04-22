f5213e982872a05d43f8191e0e5180867d68212c
DZAymen <aymen_lakehal@live.fr>
2017-04-22 14:47:23 +0100

    Add a test view to launch SUMO

--- dzTraficoBackend/dzTrafico/views.py
+++ dzTraficoBackend/dzTrafico/views.py
@@ -1,3 +1,11 @@
+from django.http import HttpResponse
 from django.shortcuts import render
-
+import traci as tr
 # Create your views here.
+def home(request):
+    tr.start(["sumo", "-c", "F:/PFE/Realisation/study-case/map.sumocfg"])
+    tr.simulationStep()
+    speed = tr.inductionloop.getLastStepMeanSpeed("1")
+    #return HttpResponse("This is the home page")
+    tr.close()
+    return HttpResponse(speed)
