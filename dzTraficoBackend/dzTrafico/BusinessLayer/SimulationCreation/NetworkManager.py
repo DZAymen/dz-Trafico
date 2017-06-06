aa94e63e75e6ab279bdd7e172b0fa9f628d562eb
DZAymen <aymen_lakehal@live.fr>
2017-06-06 01:55:16 +0100

    Add the method which returns the neighboring edge id from geoCoord / for the moment it returns the lon value until fixing spatialindex library issue

--- dzTraficoBackend/dzTrafico/BusinessLayer/SimulationCreation/NetworkManager.py
+++ dzTraficoBackend/dzTrafico/BusinessLayer/SimulationCreation/NetworkManager.py
@@ -1,17 +1,29 @@
 from dzTrafico.Helpers.MapManager import MapManager
-import subprocess
-import os
+import subprocess, os
+import sumolib
 
 class NetworkManager:
 
     __map_manager = MapManager()
+    __network_file_path = ""
+    net = None
 
     def convert_map_to_network_file(self, osm_file_path):
         #Return network file path
-        network_file_path = os.path.dirname(osm_file_path) + "\\map.net.xml"
-        subprocess.call("netconvert --osm-files " + osm_file_path + " -o " + network_file_path)
-        return network_file_path
+        self.__network_file_path = os.path.dirname(osm_file_path) + "\\map.net.xml"
+        subprocess.call("netconvert --osm-files " + osm_file_path + " -o " + self.__network_file_path)
+        return self.__network_file_path
 
     def get_network_file(self, map_box):
         self.osm_file_path = NetworkManager.__map_manager.download_map(map_box)
         return self.convert_map_to_network_file(self.osm_file_path)
+
+    def initialize_net(self):
+        if self.net is None:
+            self.net = sumolib.net.readNet(self.__network_file_path)
+
+    def get_edgeId_from_geoCoord(self, lon, lat):
+        #self.initialize_net()
+        #x, y = self.net.convertLonLat2XY(lon, lat)
+        #self.net.getNeighboringEdges(x,y,0.1, False)
+        return str(lon)
