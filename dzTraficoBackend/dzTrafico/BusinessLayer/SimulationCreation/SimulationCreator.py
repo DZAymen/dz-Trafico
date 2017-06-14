60cfe35a90635ddb6e196709b6e66ec2f5f9dd12
DZAymen <aymen_lakehal@live.fr>
2017-06-14 19:01:11 +0100

    Add sensors creation method implementation with SensorsManager

--- dzTraficoBackend/dzTrafico/BusinessLayer/SimulationCreation/SimulationCreator.py
+++ dzTraficoBackend/dzTrafico/BusinessLayer/SimulationCreation/SimulationCreator.py
@@ -2,10 +2,12 @@ from dzTrafico.BusinessEntities.Simulation import Simulation
 from NetworkManager import NetworkManager
 from dzTrafico.BusinessEntities.Sensor import Sensor
 from TripManager import TripManager
+from dzTrafico.BusinessLayer.SimulationCreation.SensorsManager import SensorsManager
 
 class SimulationCreator:
 
     __networkManager = NetworkManager()
+    __sensorsManager = SensorsManager(__networkManager)
     __tripManager = TripManager(__networkManager)
     __simulation = Simulation()
 
@@ -15,14 +17,15 @@ class SimulationCreator:
         SimulationCreator.__simulation.set_network_file(self.network_file_path)
 
     #
-    def create_sensors(self):
-        SimulationCreator.__simulation.add_sensors(Sensor("196547668#0_0", -1, 50))
-        SimulationCreator.__simulation.add_sensors(Sensor("196547668#0_1", -1, 50))
-        SimulationCreator.__simulation.add_sensors(Sensor("196547668#0_2", -1, 50))
+    def create_sensors(self, sensors_distance):
+        self.sensors, self.sensors_file = SimulationCreator.__sensorsManager.create_sensors(SimulationCreator.__simulation.get_flows(), sensors_distance)
+        SimulationCreator.__simulation.add_sensors(self.sensors)
+        SimulationCreator.__simulation.set_sensors_file(self.sensors_file)
 
     def define_traffic_flows(self, inFlowPoints, outFlowPoints):
-        self.flows_file_path = SimulationCreator.__tripManager.generate_flows_file(inFlowPoints, outFlowPoints)
+        self.flows_file_path, self.flows = SimulationCreator.__tripManager.generate_flows_file(inFlowPoints, outFlowPoints)
         self.route_file = SimulationCreator.__tripManager.generate_route_file(self.flows_file_path)
+        SimulationCreator.__simulation.set_flows(self.flows)
         SimulationCreator.__simulation.set_route_file(self.route_file)
 
     #Create Simulation Config file 'map.sumocfg'
