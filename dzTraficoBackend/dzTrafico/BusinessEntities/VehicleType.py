37b6060da75c3bab44b55c88509a097df680489c
DZAymen <aymen_lakehal@live.fr>
2017-06-14 19:27:14 +0100

    Add a constructor to VehicleType entity and a serializer

--- dzTraficoBackend/dzTrafico/BusinessEntities/VehicleType.py
+++ dzTraficoBackend/dzTrafico/BusinessEntities/VehicleType.py
@@ -1,10 +1,26 @@
+from rest_framework import serializers
+
 
 class VehicleType(object):
-    flow = 0
-    minGap = 0
-    speedFactor = 0
-    speedDev = 0
-    acceleration = 0
-    deceleration = 0
-    sigma = 0
-    tau = 0
+
+    def __init__(self, flow, minGap, speedFactor, speedDev, acceleration, deceleration, sigma, tau):
+        self.flow = flow
+        self.minGap = minGap
+        self.speedFactor = speedFactor
+        self.speedDev = speedDev
+        self.acceleration = acceleration
+        self.deceleration = deceleration
+        self.sigma = sigma
+        self.tau = tau
+
+
+class VehicleTypeSerializer(serializers.Serializer):
+
+    flow = serializers.FloatField()
+    minGap = serializers.FloatField()
+    speedFactor = serializers.FloatField()
+    speedDev = serializers.FloatField()
+    acceleration = serializers.FloatField()
+    deceleration = serializers.FloatField()
+    sigma = serializers.FloatField()
+    tau = serializers.FloatField()
