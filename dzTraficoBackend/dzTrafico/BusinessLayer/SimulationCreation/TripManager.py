54194f9bee495b0ae2fca6d76d5acf1c6b4b6a52
DZAymen <aymen_lakehal@live.fr>
2017-06-18 03:04:52 +0100

    Check if the vehicle.types.xml exists & add a vehicle_types get method to load types from xml file

--- dzTraficoBackend/dzTrafico/BusinessLayer/SimulationCreation/TripManager.py
+++ dzTraficoBackend/dzTrafico/BusinessLayer/SimulationCreation/TripManager.py
@@ -1,7 +1,7 @@
 from dzTrafico.BusinessEntities.Flow import Flow
 from dzTrafico.BusinessEntities.Simulation import Simulation
 from dzTrafico.BusinessEntities.VehicleType import VehicleType
-import subprocess
+import subprocess, os
 import lxml.etree as etree
 
 class TripManager:
@@ -72,10 +72,16 @@ class TripManager:
         return Simulation.project_directory + "\\" + self.flows_filename
 
     def add_vehicle_types(self, vehicle_types):
-        #load vehicle.types.xml file
-        root = None
+
         #if it is already created, we append the new vehicle types
-        #else, we create a new file and we append the vehicle types
+        if os.path.isfile(Simulation.project_directory + "\\..\\" + self.vehicle_types_filename):
+            # load vehicle.types.xml file
+            tree = etree.parse(Simulation.project_directory + "\\..\\" + self.vehicle_types_filename)
+            root = tree.getroot()
+        else:
+            #else, we create a new root and we append the vehicle types
+            root = etree.Element("vTypeDistribution", id="vtypedist")
+
         for vehicle_type in vehicle_types:
             type_node = etree.Element("vType",
                                       id=str(vehicle_type.type_id),
@@ -93,3 +99,28 @@ class TripManager:
         et = etree.ElementTree(root)
         et.write(Simulation.project_directory + "\\..\\" + self.vehicle_types_filename, pretty_print=True)
         return self.vehicle_types_filename
+
+    def get_vehicle_types(self):
+        if os.path.isfile(Simulation.project_directory + "\\..\\" + self.vehicle_types_filename):
+            # load vehicle.types.xml file
+            tree = etree.parse(Simulation.project_directory + "\\..\\" + self.vehicle_types_filename)
+            root = tree.getroot()
+            vtypes = root.getchildren()
+            vehicle_types = []
+            for vtype in vtypes:
+                vehicleType = VehicleType(
+                        float(vtype.get('maxSpeed')),
+                        float(vtype.get('length')),
+                        float(vtype.get('minGap')),
+                        float(vtype.get('speedFactor')),
+                        float(vtype.get('speedDev')),
+                        float(vtype.get('accel')),
+                        float(vtype.get('decel')),
+                        float(vtype.get('sigma')),
+                        float(vtype.get('tau')),
+                    )
+                vehicleType.set_type_id(vtype.get('id'))
+                vehicle_types.append(vehicleType)
+            return vehicle_types
+        else:
+            return None
