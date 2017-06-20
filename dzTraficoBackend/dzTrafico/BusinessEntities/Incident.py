792af85b6833529b13dfb95fc4656d16e6a15ffb
DZAymen <aymen_lakehal@live.fr>
2017-06-20 02:57:39 +0100

    Add incident edge_id attribute with its setter

--- dzTraficoBackend/dzTrafico/BusinessEntities/Incident.py
+++ dzTraficoBackend/dzTrafico/BusinessEntities/Incident.py
@@ -2,10 +2,15 @@ from rest_framework import serializers
 
 class Incident(object):
 
+    edge_id = ""
+
     def __init__(self, lon, lat, time):
         self.lon = lon
         self.lat = lat
         self.time = time
+
+    def set_edge_id(self, edge_id):
+        self.edge_id = edge_id
 
 class IncidentSerializer(serializers.Serializer):
 
