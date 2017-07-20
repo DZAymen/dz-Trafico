8f4c521a11b5dd7c30d5bcade1dba1504dfd8386
DZAymen <aymen_lakehal@live.fr>
2017-07-20 19:14:33 +0100

    Update JamTailProtectionAlgo by implementing is_concerned method

--- dzTraficoBackend/dzTrafico/BusinessLayer/TrafficAnalysis/JamTailProtectionControlAlgo.py
+++ dzTraficoBackend/dzTrafico/BusinessLayer/TrafficAnalysis/JamTailProtectionControlAlgo.py
@@ -6,8 +6,10 @@ class JamTailProtection:
         node = self.get_previous_node(sink, congested_node)
         while node is not None:
             if self.is_concerned(node):
-                node.set_current_max_speed(50)
+                max_speed = self.get_max_speed(node)
+                node.set_current_max_speed(max_speed)
                 vsl_nodes.append(node)
+                node = self.get_previous_node(sink, congested_node)
             else:
                 break
         return vsl_nodes
@@ -20,4 +22,11 @@ class JamTailProtection:
             return None
 
     def is_concerned(self, node):
+        if (node.initial_max_speed - node.get_current_speed()) > 20:
+            return True
         return False
+
+    def get_max_speed(self, node):
+        # node.get_current_speed()
+        # We need to define max speed values
+        return node.initial_max_speed * 0.8
