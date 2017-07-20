203688f2730125c8e13dc82f0ff49c4c634ccd3b
DZAymen <aymen_lakehal@live.fr>
2017-07-20 23:44:23 +0100

    Calculate target lane of each recommendation value

--- dzTraficoBackend/dzTrafico/BusinessEntities/LCRecommendation.py
+++ dzTraficoBackend/dzTrafico/BusinessEntities/LCRecommendation.py
@@ -5,6 +5,13 @@ class LCRecommendation:
     TURN_RIGHT = 1
     STRAIGHT_AHEAD = 0
 
+    change_lane = True
+
     def __init__(self, lane, recommendation):
         self.lane = lane
-        self.recommendation = recommendation
+        if recommendation == self.TURN_RIGHT:
+            self.target_lane = lane - 1
+        elif recommendation == self.TURN_LEFT:
+            self.target_lane = lane + 1
+        elif recommendation == self.STRAIGHT_AHEAD:
+            self.change_lane = False
