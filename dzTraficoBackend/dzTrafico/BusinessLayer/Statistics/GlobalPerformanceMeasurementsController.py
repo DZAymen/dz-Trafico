ab90e34b32aec58086149a1de2cdd14c8205fa41
DZAymen <aymen_lakehal@live.fr>
2017-07-30 17:57:19 +0100

    Add GlobalPerformanceMeasurementsController class

---
+++ dzTraficoBackend/dzTrafico/BusinessLayer/Statistics/GlobalPerformanceMeasurementsController.py
@@ -0,0 +1,9 @@
+
+class GlobalPerformanceMeasurementsController:
+
+    def __init__(self, simulation):
+        # Initialize necessary file paths
+        self.simulation = simulation
+
+    def get_results(self):
+        pass
