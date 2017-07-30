e493ef08f9d71e0f546f7636ffd4f7b15acd8712
DZAymen <aymen_lakehal@live.fr>
2017-07-30 18:39:39 +0100

    Create GlobalPerformanceMeasurement class

--- dzTraficoBackend/dzTrafico/BusinessLayer/Statistics/GlobalPerformanceMeasurementsController.py
+++ dzTraficoBackend/dzTrafico/BusinessLayer/Statistics/GlobalPerformanceMeasurementsController.py
@@ -7,3 +7,19 @@ class GlobalPerformanceMeasurementsController:
 
     def get_results(self):
         pass
+
+class GlobalPerformanceMeasurement(object):
+
+    NoControl = "no control"
+    VSL = "vsl"
+    LC = "lc"
+    VSL_LC = "vsl_lc"
+
+    def __init__(self, type, meanTravelTime, numStops, numLC, fuel, co2, nox):
+        self.type = type
+        self.meanTravelTime = meanTravelTime
+        self.numStops = numStops
+        self.numLC = numLC
+        self.fuel = fuel
+        self.co2 = co2
+        self.nox = nox
