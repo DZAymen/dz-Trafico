fc9c199ed29913c3fcfa2239fda28e230f2f08d8
DZAymen <aymen_lakehal@live.fr>
2017-07-30 19:53:46 +0100

    Calculate lanechange for VSL_LC case

--- dzTraficoBackend/dzTrafico/BusinessLayer/Statistics/GlobalPerformanceMeasurementsController.py
+++ dzTraficoBackend/dzTrafico/BusinessLayer/Statistics/GlobalPerformanceMeasurementsController.py
@@ -9,8 +9,13 @@ class GlobalPerformanceMeasurementsController:
 
     def get_results(self):
         gpms = []
-        noControlGPM = self.get_no_control_GPM()
-        gpms.append(noControlGPM)
+
+        noControl_GPM = self.get_no_control_GPM()
+        gpms.append(noControl_GPM)
+
+        vsl_lc_GPM = self.get_vsl_lc_GPM()
+        gpms.append(vsl_lc_GPM)
+
         return gpms
 
     def get_no_control_GPM(self):
@@ -26,6 +31,27 @@ class GlobalPerformanceMeasurementsController:
 
         return GlobalPerformanceMeasurement(
             GlobalPerformanceMeasurement.NoControl,
+            meanTravelTime,
+            numStops,
+            numLC,
+            fuel,
+            co2,
+            nox
+        )
+
+    def get_vsl_lc_GPM(self):
+        # Read values from summary files
+        meanTravelTime = 0
+        numStops = 0
+        numLC = self.get_num_lanechange(
+            self.simulation.lanechange_summary_vsl_lc_filename
+        )
+        fuel = 0
+        co2 = 0
+        nox = 0
+
+        return GlobalPerformanceMeasurement(
+            GlobalPerformanceMeasurement.VSL_LC,
             meanTravelTime,
             numStops,
             numLC,
