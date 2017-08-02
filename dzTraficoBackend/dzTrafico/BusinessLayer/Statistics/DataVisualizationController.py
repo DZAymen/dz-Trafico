b2a4e083a4165c95a9ae04983b6453f0575d7bbb
DZAymen <aymen_lakehal@live.fr>
2017-08-02 11:41:50 +0100

    Add DataVisualizationController class and methods to StatisticsManager

---
+++ dzTraficoBackend/dzTrafico/BusinessLayer/Statistics/DataVisualizationController.py
@@ -0,0 +1,15 @@
+
+class DataVisualizationController(object):
+
+    def __init__(self, simulation):
+        # Initialize necessary file paths
+        self.simulation = simulation
+
+    def get_emissions_results(self):
+        pass
+
+    def get_travel_time_results(self):
+        pass
+
+    def get_waiting_time_results(self):
+        pass
--- dzTraficoBackend/dzTrafico/BusinessLayer/Statistics/StatisticsManager.py
+++ dzTraficoBackend/dzTrafico/BusinessLayer/Statistics/StatisticsManager.py
@@ -1,13 +1,21 @@
 from GlobalPerformanceMeasurementsController import GlobalPerformanceMeasurementsController
+from DataVisualizationController import DataVisualizationController
 
 class StatisticsManager:
 
     def __init__(self, simulation):
         self.gpmController = GlobalPerformanceMeasurementsController(simulation)
+        self.dataVisualizationController = DataVisualizationController(simulation)
 
     def get_GPMs(self):
         gpm_results = self.gpmController.get_results()
-        for gpm in gpm_results:
-            print gpm.type, gpm.meanTravelTime
-
         return gpm_results
+
+    def get_travel_time_results(self):
+        return self.dataVisualizationController.get_travel_time_results()
+
+    def get_waiting_time_results(self):
+        return self.dataVisualizationController.get_waiting_time_results()
+
+    def get_emissions_results(self):
+        return self.dataVisualizationController.get_emissions_results()
