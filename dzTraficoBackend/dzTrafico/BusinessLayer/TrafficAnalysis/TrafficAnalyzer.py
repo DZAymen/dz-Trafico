1989e96412643866d4cde07f3ecead8fddfa0ab1
DZAymen <aymen_lakehal@live.fr>
2017-08-03 22:04:31 +0100

    Add virtualRampMeteringController call to activate vsl control

--- dzTraficoBackend/dzTrafico/BusinessLayer/TrafficAnalysis/TrafficAnalyzer.py
+++ dzTraficoBackend/dzTrafico/BusinessLayer/TrafficAnalysis/TrafficAnalyzer.py
@@ -1,25 +1,27 @@
-import sumolib
 from dzTrafico.BusinessLayer.TrafficAnalysis.JamTailProtectionControlAlgo import JamTailProtection
 from dzTrafico.BusinessLayer.TrafficAnalysis.LaneChangeControlAlgo import LaneChange
+from dzTrafico.BusinessLayer.TrafficAnalysis.VirtualRampMeteringControlAlgo import VirtualRampMetering
 
 class TrafficAnalyzer:
 
     jamTailProtectionController = JamTailProtection()
     laneChangeController = LaneChange()
+    virtualRampMeteringController = VirtualRampMetering()
 
     def __init__(self, simulation):
         self.__simulation = simulation
 
     def notify_congestion_detected(self, sink, node, congested_lanes):
 
-        vsl_nodes = self.jamTailProtectionController.get_vsl_nodes(sink, node)
-        self.activate_vsl_control(vsl_nodes)
+        # vsl_nodes = self.jamTailProtectionController.get_vsl_nodes(sink, node)
+        # self.activate_vsl_control(vsl_nodes)
 
         # if there is a free lane
         if len(node.sensors) > len(congested_lanes):
             lc_nodes = self.laneChangeController.get_lc_nodes(sink, node, congested_lanes)
+            vsl_nodes = self.virtualRampMeteringController.get_vsl_nodes(sink, node, len(lc_nodes))
+            self.activate_vsl_control(vsl_nodes)
             self.activate_lc_control(lc_nodes)
-        # nextNodeID = self.__net.getEdge(sumolib._laneID2edgeID(sensor_lane)).getToNode().getID()
 
     def activate_vsl_control(self, vsl_nodes):
 
