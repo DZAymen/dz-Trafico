9c86d7e972ab7fb733c08b49d13b323075a11df5
DZAymen <aymen_lakehal@live.fr>
2017-08-12 23:08:29 +0100

    Create EdgeStateSerializer

--- dzTraficoBackend/dzTrafico/BusinessEntities/EdgeState.py
+++ dzTraficoBackend/dzTrafico/BusinessEntities/EdgeState.py
@@ -1,4 +1,4 @@
-
+from rest_framework import serializers
 
 class EdgeState(object):
     def __init__(self, edge_id, current_speed, max_speed, vsl_state, congestion_detected):
@@ -7,3 +7,10 @@ class EdgeState(object):
         self.max_speed = max_speed
         self.vsl_state = vsl_state
         self.congestion_detected = congestion_detected
+
+class EdgeStateSerializer(serializers.Serializer):
+    edge_id = serializers.CharField()
+    current_speed = serializers.FloatField()
+    max_speed = serializers.FloatField()
+    vsl_state = serializers.BooleanField()
+    congestion_detected = serializers.BooleanField()
