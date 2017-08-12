98a2b0614ccbc7b017ab190a28c16035c1a86062
DZAymen <aymen_lakehal@live.fr>
2017-08-12 23:10:03 +0100

    Start simulation and get TrafficState data from sinks and use a serializer to convert objects to dictionary

--- dzTraficoBackend/dzTrafico/BusinessLayer/TrafficState/TrafficStateManager.py
+++ dzTraficoBackend/dzTrafico/BusinessLayer/TrafficState/TrafficStateManager.py
@@ -1,7 +1,12 @@
+from dzTrafico.BusinessLayer.SimulationManager import SimulationManager
+from dzTrafico.BusinessEntities.Simulation import Simulation
+from dzTrafico.BusinessEntities.EdgeState import EdgeStateSerializer
+import traci
 import threading
 
 class TrafficStateManager:
     __trafficStateManager = None
+    __simulationManager = SimulationManager.get_instance()
 
     @staticmethod
     def get_instance():
@@ -10,7 +15,33 @@ class TrafficStateManager:
         return TrafficStateManager.__trafficStateManager
 
     def start(self, consumer):
-        for i in range(0,5):
-            consumer.send(i)
-            threading._sleep(5)
-            print i
+        self.simulation = self.__simulationManager.get_simulation()
+        sinks = self.simulation.get_sinks()
+
+        self.simulation.start_simulation()
+        print self.simulation.sim_duration
+        for step in range(self.simulation.sim_duration):
+            traffic_state = []
+            traci.switch(self.simulation.SIM)
+            traci.simulationStep()
+
+            veh_id = self.simulation.check_incidents(step)
+            if veh_id is not None and Simulation.incident_veh is None:
+                Simulation.incident_veh = veh_id
+
+            traci.switch(self.simulation.SIM_VSL_LC)
+            traci.simulationStep()
+            self.simulation.check_incidents(step)
+
+            for sink in sinks:
+                for edgeState in sink[0].read_traffic_state():
+                    traffic_state.append(edgeState)
+
+            edgeStateSerializer = EdgeStateSerializer(traffic_state, many=True)
+            consumer.send(edgeStateSerializer.data)
+
+        traci.close()
+        traci.switch(self.simulation.SIM)
+        traci.close()
+
+        consumer.disconnect()
