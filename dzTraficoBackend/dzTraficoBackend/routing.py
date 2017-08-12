8327509271db5fd3e2f9acbd108b0034a750f620
DZAymen <aymen_lakehal@live.fr>
2017-08-12 19:45:40 +0100

    Create a route_class to map /simulation/api/realtimetrafficstate websocket path to our RealTimeTrafficStateConsumer

--- dzTraficoBackend/dzTraficoBackend/routing.py
+++ dzTraficoBackend/dzTraficoBackend/routing.py
@@ -1,1 +1,6 @@
-channel_routing = {}
+from dzTrafico.Consumers.RealTimeTrafficState import RealTimeTrafficStateConsumer
+from channels.routing import route, route_class
+
+channel_routing = [
+    route_class(RealTimeTrafficStateConsumer, path=r"^/simulation/api/realtimetrafficstate/$")
+]
