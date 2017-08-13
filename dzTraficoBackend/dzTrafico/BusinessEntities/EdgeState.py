71557a3417ba4242569b300cf742b25b2a531c05
DZAymen <aymen_lakehal@live.fr>
2017-08-13 18:26:01 +0100

    Add density to real time data

--- dzTraficoBackend/dzTrafico/BusinessEntities/EdgeState.py
+++ dzTraficoBackend/dzTrafico/BusinessEntities/EdgeState.py
@@ -1,10 +1,11 @@
 from rest_framework import serializers
 
 class EdgeState(object):
-    def __init__(self, edge_id, current_speed, max_speed, vsl_state, congestion_detected):
+    def __init__(self, edge_id, current_speed, max_speed, density, vsl_state, congestion_detected):
         self.edge_id = edge_id
         self.current_speed = current_speed
         self.max_speed = max_speed
+        self.density = density
         self.vsl_state = vsl_state
         self.congestion_detected = congestion_detected
 
@@ -12,5 +13,6 @@ class EdgeStateSerializer(serializers.Serializer):
     edge_id = serializers.CharField()
     current_speed = serializers.FloatField()
     max_speed = serializers.FloatField()
+    density = serializers.FloatField()
     vsl_state = serializers.BooleanField()
     congestion_detected = serializers.BooleanField()
