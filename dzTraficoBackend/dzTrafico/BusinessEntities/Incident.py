b3b99eb1334d7ecc980e538191c791079278b48b
DZAymen <aymen_lakehal@live.fr>
2017-08-16 10:33:40 +0100

    Add lane attribute to incident entity

--- dzTraficoBackend/dzTrafico/BusinessEntities/Incident.py
+++ dzTraficoBackend/dzTrafico/BusinessEntities/Incident.py
@@ -5,12 +5,13 @@ class Incident(object):
 
     lanes = []
 
-    def __init__(self, lon, lat, accidentTime, accidentDuration):
+    def __init__(self, lon, lat, accidentTime, accidentDuration, lane):
         self.lon = lon
         self.lat = lat
         self.position = Location(lon, lat)
         self.accidentTime = accidentTime
         self.accidentDuration = accidentDuration
+        self.lane = lane
 
     def set_lanes(self, lanes):
         self.lanes = lanes
@@ -19,11 +20,13 @@ class IncidentSerializer(serializers.Serializer):
     position = LocationSerializer()
     accidentTime = serializers.FloatField()
     accidentDuration = serializers.FloatField()
+    lane = serializers.IntegerField()
 
     def create(self, validated_data):
         return Incident(
             validated_data["position"]["lng"],
             validated_data["position"]["lat"],
             validated_data["accidentTime"],
-            validated_data["accidentDuration"]
+            validated_data["accidentDuration"],
+            validated_data["lane"]
         )
